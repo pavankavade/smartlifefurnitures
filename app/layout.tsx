09146d92e522edf08e697f34265063b1faeec66c
Pavan Kavade <pavan1kavade@gmail.com>
2026-01-10 17:43:15 +0530

    t

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-export const runtime = 'edge';
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
